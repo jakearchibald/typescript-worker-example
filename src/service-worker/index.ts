f8ec7c4f73c345eedd5de2f5d1b6f5571c8a38ed
Jake Archibald <jaffathecake@gmail.com>
2019-05-29 18:23:56 +0100

    'Fixing' global issue

--- src/service-worker/index.ts
+++ src/service-worker/index.ts
@@ -1,7 +1,11 @@
 // This file must have worker types, but not DOM types.
 // The global should be that of a service worker.
-console.log(clients);
 
-addEventListener('fetch', (event) => {
+// Ugh, you have to do this, and use `global.whatever` rather than `whatever`.
+const global = self as ServiceWorkerGlobalScope;
+
+console.log(global.clients);
+
+global.addEventListener('fetch', (event) => {
   console.log(event.request);
 });
