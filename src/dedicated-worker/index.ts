7d2b53e6bf2e63f18e0fb452cf95ac33039ba567
Jake Archibald <jaffathecake@gmail.com>
2019-05-30 08:18:14 +0100

    Fixing dedicated worker self

--- src/dedicated-worker/index.ts
+++ src/dedicated-worker/index.ts
@@ -1,10 +1,15 @@
 // This file must have worker types, but not DOM types.
 // The global should be that of a dedicated worker.
 
+// This fixes `self`'s type.
+declare var self: DedicatedWorkerGlobalScope;
+
 const helloMessage = {
   hello: 'world',
 };
 
 export type HelloMessage = typeof helloMessage;
 
+// Both of these should work.
 postMessage(helloMessage);
+self.postMessage(helloMessage);
